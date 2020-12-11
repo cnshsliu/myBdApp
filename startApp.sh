f83a4518e9892d92f9e72f9db5c1657caecd3fcd
lucas liu@bdi_virtualbox <liukehong@gmail.com>
2020-12-12 06:42:45 +0800

    added hadoop-hive.env

--- startApp.sh
+++ startApp.sh
@@ -1,6 +1,7 @@
 docker run --rm \
   --name my-app-1 \
   -e ENABLE_INIT_DAEMON=false \
+  --env-file ./config/hadoop-hive.env \
   --network bdi_net_lucas \
   --link spark-master:spark-master \
   lucas/myapp
